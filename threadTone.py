ad3becefc36db0370241a953c20ca18d75bd0a9c
Eric Rasche <rasche.eric@gmail.com>
2017-01-07 10:04:12 +0000

    Greyer

--- threadTone.py
+++ threadTone.py
@@ -8,7 +8,7 @@ imgRadius = 500     # Number of pixels that the image radius is resized to
 
 initPin = 0         # Initial pin to start threading from
 nPins = 250         # Number of pins on the circular loom
-nLines = 2000        # Maximal number of lines
+nLines = 3000        # Maximal number of lines
 
 minLoop = 3         # Disallow loops of less than minLoop lines
 lineWidth = 3       # The number of pixels that represents the width of a thread
